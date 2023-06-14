7af25d3358cfc201a27e78343e8ac9f67edd3661
daniela <moreno.tobar.daniela@gmail.com>
2023-06-14 18:26:29 -0500

    tabla de multiplicar

---
+++ src/main/java/com/project100/exercises/TablaMultiplicar.java
@@ -0,0 +1,24 @@
+package com.project100.exercises;
+
+import java.util.Scanner;
+
+public class TablaMultiplicar {
+
+    /* Pedir un numero por consola y mostrar la tabla del 10
+     */
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("\n");
+
+        System.out.println("Escribe un numero");
+        int numero = sc.nextInt();
+
+        int resultado = 0;
+        for (int i = 1; i <= 10; i++) {
+            resultado = i * numero;
+            System.out.println(numero + " x " + i + " = " + resultado);
+        }
+
+    }
+}
