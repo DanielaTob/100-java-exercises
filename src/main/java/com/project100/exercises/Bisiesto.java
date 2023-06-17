48390bb7b22bce6c487b7338fc558314ae57ddd2
daniela <moreno.tobar.daniela@gmail.com>
2023-06-17 13:33:07 -0500

    new exercises

---
+++ src/main/java/com/project100/exercises/Bisiesto.java
@@ -0,0 +1,38 @@
+package com.project100.exercises;
+
+import java.util.Scanner;
+
+public class Bisiesto {
+
+    /* Pide un año por teclado (numero) e indica si es bisisesto o no.
+       Es bisiesto cuando divisible entre 4 y no divisible entre 100 o si es divisible entre 400
+     */
+
+    /* && y
+        v & v = v
+        v & f = f
+        f & v = f
+        f & f = f
+     */
+
+    /* || or
+        v | v = v
+        v | f = v
+        f | v = v
+        f | f = f
+     */
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escribe un numero");
+        int bisiesto = sc.nextInt();
+
+        if ( bisiesto % 4 == 0 && bisiesto % 100 != 0 || bisiesto % 400 == 0){
+            System.out.println("Es año bisiesto");
+        }else{
+            System.out.println("No es año bisiesto");
+        }
+    }
+}
---
+++ src/main/java/com/project100/exercises/ValidarNumero.java
@@ -0,0 +1,29 @@
+package com.project100.exercises;
+
+import java.util.Scanner;
+
+public class ValidarNumero {
+
+    /* Pide un numero por consola y valida que ese numero este entre 0 y 10.
+     */
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        int numero;
+        do {
+
+            System.out.println("Escribe un numero");
+            numero = sc.nextInt();
+
+            if ( !(numero >= 0 && numero <= 10) ){
+                System.out.println("Digita un numero entre 0 y 10");
+            }
+
+        }while( !(numero >= 0 && numero <= 10) );
+
+        System.out.println("El numero " + numero + " esta entre 0 y 10");
+    }
+}
